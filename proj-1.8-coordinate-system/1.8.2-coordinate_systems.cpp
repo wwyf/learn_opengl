49f3e0e29d0ad333564c05aaba91ecf960a4247d
wangyf <walkeryf@yeah.net>
2018-05-01 12:36:58 +0800

    add 18.2

--- proj-1.8-coordinate-system/1.8.2-coordinate_systems.cpp
+++ proj-1.8-coordinate-system/1.8.2-coordinate_systems.cpp
@@ -51,6 +51,8 @@ int main()
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     // build and compile our shader zprogram
     // ------------------------------------
@@ -199,10 +201,11 @@ int main()
         ourShader.use();
 
         // create transformations
-        glm::mat4 model;
-        glm::mat4 view;
-        glm::mat4 projection;
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        // model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
