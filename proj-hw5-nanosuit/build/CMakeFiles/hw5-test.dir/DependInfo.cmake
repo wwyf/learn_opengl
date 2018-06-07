118dabd663016dececbd1019357aa278cf583079
walker <WalkerYF@yeah.net>
2018-06-07 17:56:05 +0800

    测试了纳米装

---
+++ proj-hw5-nanosuit/build/CMakeFiles/hw5-test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/0code/learn_opengl/proj-hw5-nanosuit/source/model_loading.cpp" "E:/0code/learn_opengl/proj-hw5-nanosuit/build/CMakeFiles/hw5-test.dir/source/model_loading.cpp.obj"
+  "E:/0code/learn_opengl/proj-hw5-nanosuit/source/stb_image.cpp" "E:/0code/learn_opengl/proj-hw5-nanosuit/build/CMakeFiles/hw5-test.dir/source/stb_image.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "E:/0code/learn_opengl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proj-hw5-nanosuit/build/CMakeFiles/hw5-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw5-test.dir/source/model_loading.cpp.obj"
+  "CMakeFiles/hw5-test.dir/source/stb_image.cpp.obj"
+  "hw5-test.pdb"
+  "hw5-test.exe"
+  "hw5-test.exe.manifest"
+  "libhw5-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw5-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
