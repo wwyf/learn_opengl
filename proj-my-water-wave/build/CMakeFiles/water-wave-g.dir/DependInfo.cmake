720ecbc98e7fc36c4ea027cac4cc4028977194c1
walker <WalkerYF@yeah.net>
2018-06-19 13:08:54 +0800

    update git

---
+++ proj-my-water-wave/build/CMakeFiles/water-wave-g.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/0code/learn_opengl/proj-my-water-wave/source/gerstner-water.cpp" "E:/0code/learn_opengl/proj-my-water-wave/build/CMakeFiles/water-wave-g.dir/source/gerstner-water.cpp.obj"
+  "E:/0code/learn_opengl/proj-my-water-wave/source/util.cpp" "E:/0code/learn_opengl/proj-my-water-wave/build/CMakeFiles/water-wave-g.dir/source/util.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proj-my-water-wave/build/CMakeFiles/water-wave-g.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/water-wave-g.dir/source/gerstner-water.cpp.obj"
+  "CMakeFiles/water-wave-g.dir/source/util.cpp.obj"
+  "water-wave-g.pdb"
+  "water-wave-g.exe"
+  "water-wave-g.exe.manifest"
+  "libwater-wave-g.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/water-wave-g.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
